f0af0fb02ddeff0930aa79659a88d5c346e01084
Will Hodges <hodges.wt@gmail.com>
2015-10-23 11:31:05 -0400

    Added comments
    Added comments to explain, removed an unnecessary import

--- imp.py
+++ imp.py
@@ -4,43 +4,43 @@
 	
 '''
 
-from calc import *
-import json
+from calc import * # Import the parser!
 
-parser = calcParser()
+parser = calcParser() # Create an instance of the parser!
 
-print "\n\n     Will Hodges Python Interpreter Calculator 10/23/15"
+print "\n\n     Will Hodges Python Interpreter Calculator 10/23/15" # Greet the user
 
-print "\n\nWelcome to the WHPIC! Enter an expression (or series of expressions) below.\n\nSeperate each by a semicolon and nothing more!\n"
+print "\n\nWelcome to the WHPIC! Enter an expression (or series of expressions) \
+		below.\n\nSeperate each by a semicolon and nothing more!\n"  # Explain to the user
 
-user_input = raw_input("Equation: ")
+user_input = raw_input("Equation: ") # Get the equation from the user
 
-ast = parser.parse(user_input, rule_name='start')
+ast = parser.parse(user_input, rule_name='start') # Parse the equation (easy, right?)
 
-def eval(expr):
-	operator = expr[1][0]
-	op1arr = expr[0][0]
-	op1str = ''
-	for num in op1arr:
-		op1str = op1str + num
-	op1 = int(op1str)
+def eval(expr): # Create the evaluator of the expression
+	operator = expr[1][0] # Get the operator
+	op1arr = expr[0][0] # Get the first operand (multi-digit numbers are split into characters) 
+	op1str = '' # Blank for now...
+	for num in op1arr: # For each character
+		op1str = op1str + num # Append it to op1str
+	op1 = int(op1str) # convert op1str to an integer for use later
 	
-	op2arr = expr[2][0]
-	op2str = ''
-	for num in op2arr:
-		op2str = op2str + num
-	op2 = int(op2str)
+	op2arr = expr[2][0] # Get the second operand (multi-digit numbers are split into characters) 
+	op2str = '' # Blank for now
+	for num in op2arr: # For each character
+		op2str = op2str + num # Append it to op2str
+	op2 = int(op2str) # convert op2str to an integer for use later
 	
-	if operator == '+':
-		print "\n" + str(op1) + " + " + str(op2) + " = " + str((op1 + op2))
-	elif operator == '-':
-		print "\n" + str(op1) + " - " + str(op2) + " = " + str((op1 - op2))
-	elif operator == '/':
-		print "\n" + str(op1) + " / " + str(op2) + " = " + str((op1 / op2))
-	elif operator == '*':
-		print "\n" + str(op1) + " * " + str(op2) + " = " + str((op1 * op2))
+	if operator == '+': # If addition 
+		print "\n" + str(op1) + " + " + str(op2) + " = " + str((op1 + op2)) # Nicely print the problem and the answer
+	elif operator == '-': # If subtraction
+		print "\n" + str(op1) + " - " + str(op2) + " = " + str((op1 - op2)) # Nicely print the problem and the answer
+	elif operator == '/': # If division
+		print "\n" + str(op1) + " / " + str(op2) + " = " + str((op1 / op2)) # Nicely print the problem and the answer
+	elif operator == '*': # If multiplication
+		print "\n" + str(op1) + " * " + str(op2) + " = " + str((op1 * op2)) # Nicely print the problem and the answer
 
-composite_list = [ast[x:x+4] for x in range(0, len(ast),4)]
-for expr in composite_list:
-	eval(expr)
-print "\n"
+composite_list = [ast[x:x+4] for x in range(0, len(ast),4)] # Split the parsed info into separate equations
+for expr in composite_list:	# For each expression			# (one equations contains: operand1, operator, operand2,
+	eval(expr)				# Evaluate it!					# and a semicolon
+print "\n" # Make a buffer between our info and the chatter from the terminal
